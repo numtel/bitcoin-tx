c1c50339746d0b20cc62885d5cc2a992cbc3bfcb
Ben Green <bgreen787@gmail.com>
2013-04-17 15:19:09 -0700

    Bitcoin deposits are now pending with less than 6 confirmations

--- index.php
+++ index.php
@@ -53,16 +53,22 @@ if(
 			$btc_update_query->execute(array($_GET['transaction_hash']));
 			die('*ok*');
 		}else{
-			//create pending item
-			$btc_insert_query=$db->prepare('insert into `tx` (`fb_id`,`type`,`btc_addr`,`date`,`amount`,`hash`) values (?, ?, ?, ?, ?, ?)');
-			$btc_insert_query->execute(array(
-				$_GET['fb_id'],
-				'4',
-				$_GET['input_address'],
-				date('Y-m-d H:i:s'),
-				$_GET['value'],
-				$_GET['transaction_hash']
-			));
+			//make sure not already inserted
+			$test_insert_query=$db->prepare('select * from `tx` where `hash` = ?');
+			$test_insert_query->execute(array($_GET['transaction_hash']));
+			$test_insert_data=$test_insert_query->fetchAll();
+			if(count($test_insert_data)===0){
+				//create pending item
+				$btc_insert_query=$db->prepare('insert into `tx` (`fb_id`,`type`,`btc_addr`,`date`,`amount`,`hash`) values (?, ?, ?, ?, ?, ?)');
+				$btc_insert_query->execute(array(
+					$_GET['fb_id'],
+					'4',
+					$_GET['input_address'],
+					date('Y-m-d H:i:s'),
+					$_GET['value'],
+					$_GET['transaction_hash']
+				));
+			}
 			die('');
 		}
 	}else{
